a59f3c88b948660ebdce813c3eb2297545e1595a
r-yeh <r.yeh2008@gmail.com>
2025-07-08 16:34:00 -0400

    initialized packages and set initial home page

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../styles/Home.css';
+
+const Home = () => {
+  return (
+      <div className="container">
+          <h1>Home Page</h1>
+      </div>
+  );
+};
+
+export default Home;
