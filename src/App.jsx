51d860f942c6503de23c422cf0b57ca72248c994
r-yeh <r.yeh2008@gmail.com>
2025-07-21 16:14:18 -0400

    init experience page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {ThemeProvider} from '@mui/material/styles';
 import theme from './assets/themes/Theme';
 import './styles/App.css'
 import Home from './pages/Home';
+import Experience from "./pages/Experience/index.jsx";
 import Projects from './pages/Projects';
 import About from './pages/About';
 import NavBar from './components/nav-bar/NavBar';
@@ -15,6 +16,7 @@ function App() {
                 <NavBar/>
                 <Routes>
                     <Route path="/" element={<Home/>}/>
+                    <Route path="/experience" element={<Experience/>}/>
                     <Route path="/projects" element={<Projects/>}/>
                     <Route path="/about" element={<About/>}/>
                 </Routes>
