d49df5c3d63f41f78fafdcdc4c2dccfdfb900cea
r-yeh <r.yeh2008@gmail.com>
2025-07-21 16:02:34 -0400

    changed typography variant to match documentation

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,13 +7,28 @@ import DownloadIcon from '@mui/icons-material/Download';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import Typewriter from "typewriter-effect";
-import { motion } from "motion/react"
+import {stagger} from "motion";
+import {motion} from "motion/react"
 
 import '../../styles/Home.css';
 import resume from '../../assets/files/resume.pdf';
-import { github_url, linkedin_url } from "../../constants/index.js";
+import {github_url, linkedin_url} from "../../constants/index.js";
 
 const Home = () => {
+    const iconVariants = {
+        hidden: {scale: 0, opacity: 0, x: -50},
+        show: {scale: 1, opacity: 1, x: 0, transition: {duration: 0.8}},
+    };
+
+    const containerVariants = {
+        hidden: {},
+        show: {
+            transition: {
+                delayChildren: stagger(0.25),
+            },
+        },
+    };
+
     const onResumeDownload = () => {
         const link = document.createElement('a');
         link.href = resume;
@@ -41,7 +56,7 @@ const Home = () => {
                         />
                     </Typography>
                 </Typography>
-                <Typography variant="p" sx={{marginTop: "5%"}}>
+                <Typography variant="body1" sx={{marginTop: "5%"}}>
                     Welcome to my portfolio! I am a recent Software Engineering graduate from McMaster University
                     currently searching for new opportunities to grow my skills and develop my career.
                 </Typography>
@@ -51,22 +66,23 @@ const Home = () => {
                         size="large"
                         startIcon={<DownloadIcon/>}
                         component={motion.button}
-                        whileHover={{ scale: 1.05 }}
-                        whileTap={{ scale: 0.95 }}
+                        whileHover={{scale: 1.05}}
+                        whileTap={{scale: 0.95}}
                         onClick={onResumeDownload}
                 >
                     Download Resume
                 </Button>
             </div>
-            <div className='icons'>
+            <motion.div className='icons' variants={containerVariants} initial="hidden" animate="show">
                 <Link target="_blank" rel="noopener noreferrer" href={github_url}>
                     <IconButton
                         children={<GitHubIcon/>}
                         size="large"
                         color="tertiary"
                         component={motion.button}
-                        whileHover={{ scale: 1.05 }}
-                        whileTap={{ scale: 0.95 }}
+                        variants={iconVariants}
+                        whileHover={{scale: 1.05}}
+                        whileTap={{scale: 0.95}}
                     />
                 </Link>
                 <Link target="_blank" rel="noopener noreferrer" href={linkedin_url}>
@@ -75,11 +91,12 @@ const Home = () => {
                         size="large"
                         color="tertiary"
                         component={motion.button}
-                        whileHover={{ scale: 1.05 }}
-                        whileTap={{ scale: 0.95 }}
+                        variants={iconVariants}
+                        whileHover={{scale: 1.05}}
+                        whileTap={{scale: 0.95}}
                     />
                 </Link>
-            </div>
+            </motion.div>
         </div>
     );
 };
