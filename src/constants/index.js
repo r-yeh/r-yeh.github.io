97f2a68189828efa6626c0efae95459cd46ad918
r-yeh <r.yeh2008@gmail.com>
2025-07-24 16:38:40 -0400

    changed wording of experience bullet points

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,7 +4,7 @@ export const linkedin_url = "https://www.linkedin.com/in/ryan-yeh-2008/";
 
 export const opg_experience = [
     "Spearheaded the development of a web dashboard that streamlined tasks performed by the Fuel Handling Operations team by consolidating critical data for improved accessibility and efficiency using Angular/ASP.NET.",
-    "Created dynamic Power BI dashboards, enabling tracking and analysis of key metrics, including project spending",
+    "Created 3 dynamic Power BI dashboards, enabling tracking and analysis of key metrics, including project spending",
     "Automated complex, repetitive tasks by implementing VBA macros in Excel spreadsheets, reducing manual effort and increasing productivity"
 ];
 
@@ -12,15 +12,14 @@ export const ministry_experience = [
     "Built and maintained a web application to help Ontario healthcare facilities create and manage financial templates and data using the MERN stack",
     "Led development of platform organization management functionality and corresponding database CRUD resulting in streamlined user workflows",
     "Enhanced database query performance related to organization management, resulting in a 30% improvement in retrieval speed",
-    "Developed and maintained a Postman test suite to ensure backend reliability and functionality",
+    "Developed and maintained a Postman test suite consisting of 15+ tests to ensure backend reliability and functionality",
     "Led discussion during daily stand-ups, enhancing team collaboration"
 ];
 
 export const sentry_health_experience = [
-    "Acted as the primary QA tester, conducting manual tests and executing automated test suites for each deployment and update",
-    "Developed automated Cypress and Postman tests, reducing QA workload and streamlining the testing process",
-    "Led development of a web application for managing an internal pharmacy database, enabling staff to easily CRUD Firestore data using Vue/Vuetify for the frontend and GraphQL for the backend",
-    "Contributed to an application that scrapes the College of Physicians and Surgeons of Ontario database to maintain a BigQuery table of provincial physician information for use in company projects using JavaScript and Python",
+    "Developed and maintained 30+ regression tests using Cypress and Postman, reducing QA workload and streamlining the testing process",
+    "Conducted manual unit and integration testing on 20+ work items per sprint",
+    "Led development of a web application for managing an internal pharmacy database with 100+ records, enabling staff to easily CRUD Firestore data using Vue/Vuetify for the frontend and GraphQL for the backend",
     "Built an application to convert PDFs to base64 and send data to an online fax endpoint, reducing manual workload for clinic staff using Node.js",
     "Automated file uploads to Firestore by creating a Node-RED workflow that monitors and processes new files, eliminating manual administrative tasks",
     "Used Jira to track bugs and manage development tasks, improving team visibility and ensuring faster issue resolution during sprints"
