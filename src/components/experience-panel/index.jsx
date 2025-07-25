4c4e04ce278e0afca9ca935009a1a305158901dd
r-yeh <r.yeh2008@gmail.com>
2025-07-25 15:59:14 -0400

    added animation on job experience bullet points

--- src/components/experience-panel/index.jsx
+++ src/components/experience-panel/index.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
 import Box from '@mui/material/Box';
+import {motion} from "motion/react";
+import {stagger} from "motion";
 
 import '../../styles/ExperiencePanel.css';
 import Typography from "@mui/material/Typography";
 
 const ExperiencePanel = (props) => {
     const {title, duration, points, value, index, ...other} = props;
+
+    const [renderKey, setRenderKey] = React.useState(Date.now());
+
+    React.useEffect(() => {
+        if (value === index) {
+            setRenderKey(Date.now());
+        }
+    }, [value, index]);
+
+    const listItemVariants = {
+        hidden: {
+            opacity: 0,
+            x: -50,
+        },
+        show: {
+            opacity: 1,
+            x: 0,
+            transition: {
+                duration: 0.6,
+            },
+        },
+    };
+
+    const listVariants = {
+        hidden: {},
+        show: {
+            transition: {
+                delayChildren: stagger(0.25),
+            },
+        }
+    };
 
     return (
         <div
@@ -18,13 +51,19 @@ const ExperiencePanel = (props) => {
             <Box sx={{p: 3, flexGrow: 1, overflowY: 'auto'}}>
                 <Typography variant='h3'>{title}</Typography>
                 <Typography sx={{marginTop: '1rem'}} variant='h6'>{duration}</Typography>
-                <ul className='experience-list'>
+                <motion.ul
+                    key={renderKey}
+                    className='experience-list'
+                    variants={listVariants}
+                    initial="hidden"
+                    animate="show"
+                >
                     {points.map((point, index) => (
-                        <li key={index} className='experience-list-item'>
+                        <motion.li key={index} className='experience-list-item' variants={listItemVariants}>
                             <Typography variant='body2'>{point}</Typography>
-                        </li>
+                        </motion.li>
                     ))}
-                </ul>
+                </motion.ul>
             </Box>
         </div>
     );
