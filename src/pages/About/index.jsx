00a7c89518b0b0d66d19a6143e0df132a5637f71
r-yeh <r.yeh2008@gmail.com>
2025-08-01 16:29:27 -0400

    added image to about page and fixed page formatting

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -4,19 +4,33 @@ import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 
 import '../../styles/About.css'
+import AboutPicture from '../../assets/images/about-picture.jpg';
 
 const About = () => {
     return (
         <div className='about-page'>
-            <Grid container>
-                <Grid size={{xs: 12, md: 3}}>
-                    <Paper>
-                        <Typography variant="h1">Image here</Typography>
-                    </Paper>
+            <Grid container spacing={2}>
+                <Grid size={{xs: 12, md: 4}}>
+                    <div style={{
+                        display: 'flex',
+                        width: '100%',
+                        height: '84vh',
+                        justifyContent: 'center',
+                        alignItems: 'center'
+                    }}>
+                        <img style={{maxHeight: '100%', maxWidth: '100%', objectFit: 'contain', borderRadius: '8px'}}
+                             src={AboutPicture} alt='Picture of Me'/>
+                    </div>
                 </Grid>
-                <Grid size={{xs:12, md: 9}}>
-                    <Paper>
-                        <Typography variant="h1">Description here</Typography>
+                <Grid size={{xs: 12, md: 8}}>
+                    <Paper sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        height: '84vh'
+                    }}>
+                        <Typography variant="h2">Description here</Typography>
                     </Paper>
                 </Grid>
             </Grid>
