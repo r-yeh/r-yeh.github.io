6a152996e46c1d2b8fbe4d75c1cda24a8d7b6bd2
r-yeh <r.yeh2008@gmail.com>
2025-08-18 16:25:38 -0400

    fixed grid syntax and position styling

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
 
 import '../../styles/Projects.css';
 import ProjectCard from '../../components/project-card/index.jsx';
-import Grid from "@mui/material/Grid";
 
 const Projects = () => {
     return (
-        <div>
-            <Typography variant="h1">Projects</Typography>
-            <Grid spacing={2} container columns={12}>
-                <Grid item xs={12} md={4}>
+        <div className='project-page'>
+            <Grid spacing={2} container columns={12} sx={{width: '100%'}}>
+                <Grid size={{xs: 12, md: 6}}>
                     <ProjectCard
                         title='Project 1'
+                        image='https://blocks.astratic.com/img/general-img-landscape.png'
+                        description='This is a description of the project.'
+                        url='https://google.com'/>
+                </Grid>
+                <Grid size={{xs: 12, md: 6}}>
+                    <ProjectCard
+                        title='Project 2'
                         image='https://blocks.astratic.com/img/general-img-landscape.png'
                         description='This is a description of the project.'
                         url='https://google.com'/>
