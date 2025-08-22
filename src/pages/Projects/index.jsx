fe3a28d817191b31045a69a548cce49352595b43
r-yeh <r.yeh2008@gmail.com>
2025-08-22 16:55:25 -0400

    animation on appearance of project cards

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Grid from "@mui/material/Grid";
+import {motion} from "motion/react";
 
 import '../../styles/Projects.css';
 import ProjectCard from '../../components/project-card/index.jsx';
@@ -10,7 +11,13 @@ const Projects = () => {
         <div className='project-page'>
             <Grid spacing={2} container columns={12} sx={{height: '50%', width: '100%'}}>
                 {projects.map((project, index) => (
-                    <Grid size={{xs: 12, md: 6}} key={index}>
+                    <Grid
+                        component={motion.div}
+                        initial={{opacity: 0, y: 50}}
+                        animate={{opacity: 1, y: 0, transition: {duration: 0.7, delay: index * 0.15}}}
+                        size={{xs: 12, md: 6}}
+                        key={index}
+                    >
                         <ProjectCard
                             title={project.title}
                             image={project.image}
